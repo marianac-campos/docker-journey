9824523bbf4aff316659e87afc57b1648e351897
marianac-campos <marianacristinadecampos@gmail.com>
2023-01-17 19:55:47 -0300

    improving my index.js

--- alura-first-steps/tst-app-node/src/index.js
+++ alura-first-steps/tst-app-node/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 let app = express();
 
+const PORT_BUILD = process.env.PORT_BUILD
+
 app.use(express.static("."));
 
 app.get("/", (req,res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 
-app.listen("3000", ()=>{
-    console.log("Server is listening on port 3000")
+app.listen(PORT_BUILD, ()=>{
+    console.log(`Server is listening on port ${PORT_BUILD}`)
 })
